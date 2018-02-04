d176a9d863833a92887486c4289301c555c10cf4
Utkarsh Gupta <utkarshg1408@gmail.com>
2018-02-05 00:11:18 +0530

    Added Pub Nub

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+config.db = {};
+
+config.db.name = 'tf1';
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,43 +1,81 @@
-const
-express = require("express")
-bodyParser = require('body-parser')
-MongoClient = require('mongodb').MongoClient
+var express = require("express");
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var app = express();
+var path = require('path');
+var config = require('./config.js');
+var date = new Date();
+var PubNub = require('pubnub');
 
-var url = "mongodb://utkarsh:moneyball@ds147864.mlab.com:47864/heroku_w3mw9ds3"
-var app = express()
+var pubnub = new PubNub({
+  publishKey : 'pub-c-b05337d3-6133-4097-9fcd-d2acdfdd9e77',
+  subscribeKey : 'sub-c-22ea1e5a-099b-11e8-8425-92777572ae45'
+});
+
+var db_url = "mongodb://localhost:27017";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(request, response, next) {
-	response.send("Welcome to Homepage!");
+	response.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+var client;
+
+MongoClient.connect(db_url, function(err, cli) {
+  if (err) throw err;
+  client = cli;
+  console.log("DB Connected");
 });
 
 app.post("/post_data", function(request, response) {
-  console.log(request.body)
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    
-    var timestamp = request.body.timestamp
-    var current = request.body.current
-    var temperature = request.body.temperature
-    var voltage = request.body.voltage
-    var moisture_content = request.body.moisture_content
+  var db = client.db(config.db.name);
+  var timestamp = request.body.timestamp;
+  var current = request.body.current;
+  var temperature = request.body.temperature;
+  var voltage = request.body.voltage;
+  var moisture_content = request.body.moisture_content;
 
-    /* Insert element in database */
+  /* Publish data to PubNub to dynamically vizualise sensor values */
+  var publish_data = {
+    'voltage': voltage,
+    'current': current,
+    'temperature': temperature,
+    'moisture_content': moisture_content
+  };
 
-    db.collection('tf1').insert({
-      'timestamp': timestamp,
-      'current': current,
-      'temperature': temperature,
-      'voltage': voltage,
-      'moisture_content': moisture_content
-    });
+  var publishConfig = {
+    channel : 'transformer_data',
+    message: {"eon": publish_data}
+  };
 
-    response.end("POST Request Successful!");
+  pubnub.publish(publishConfig, function (status, response) {
+    if (status.error) {
+        console.log("PubNub Error ", status)
+    } else {
+        console.log("message Published w/ timetoken", response.timetoken)
+    }
   });
+
+  /* Insert element in database */
+  var insert_data = {
+    'timestamp': timestamp,
+    'current': current,
+    'temperature': temperature,
+    'voltage': voltage,
+    'moisture_content': moisture_content
+  };
+
+  db.collection('tf1').insert(insert_data, function(err){
+      if(err) throw err;
+      console.log("Inserted");
+  });
+
+  response.end("POST Request Successful!");
 });
 
 app.get("*", function(request, response) {
