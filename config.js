3260a2a040051ab71724ea55b61d0fc4209fdf29
Utkarsh Gupta <utkarshg1408@gmail.com>
2018-02-05 19:43:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
-config.webhost = 'https://slash-url.herokuapp.com/';
+config.webhost = 'CUSTOM_WEBHOST';
 
 config.db = {};
 config.db.name = 'heroku_5pxr3rhr';
@@ -7,7 +7,7 @@ config.db.localhost = 'mongodb://localhost';
 config.db.port = 27017;
 
 config.pubsub = {};
-config.pubsub.publishKey = 'pub-c-b05337d3-6133-4097-9fcd-d2acdfdd9e77';
-config.pubsub.subscribeKey = 'sub-c-22ea1e5a-099b-11e8-8425-92777572ae45';
+config.pubsub.publishKey = 'YOUR_PUBSUB_PUBLISHKEY';
+config.pubsub.subscribeKey = 'YOUR_PUBSUB_SUBSCRIBEKEY';
 
 module.exports = config;
